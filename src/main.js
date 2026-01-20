ee332f69f5d1d365ce8cb756d94bdce4ede61c74
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-01-20 11:03:50 +0900

    回転アニメーション

--- src/main.js
+++ src/main.js
@@ -1,31 +1,37 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 function main() {
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-	const canvas = document.querySelector( '#c' );
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-	const fov = 75;
-	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+  const scene = new THREE.Scene();
 
-	const scene = new THREE.Scene();
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
 
-	const material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } ); // greenish blue
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+  function render(time) {
+    time *= 0.001;
+    cube.rotation.x = time;
+    cube.rotation.y = time;
 
-	renderer.render( scene, camera );
-
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 main();
