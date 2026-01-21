ceb0f288ba87a0166530c185f588d7550388c6af
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-01-21 09:50:58 +0900

    ブロックノイズの修正

--- src/main.js
+++ src/main.js
@@ -42,15 +42,28 @@ function main() {
     makeInstance(geometry, 0xaa8844, 2),
   ];
 
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
   function render(time) {
     time *= 0.001;
 
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
 
     cubes.forEach((cube, ndx) => {
-      const speed = 1 + ndx * .1;
+      const speed = 1 + ndx * 0.1;
       const rot = time * speed;
       cube.rotation.x = rot;
       cube.rotation.y = rot;
